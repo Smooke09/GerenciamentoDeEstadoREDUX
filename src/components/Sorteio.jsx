9775008c32953aa37dffbe1f9e1a2241a30811a9
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-21 12:42:20 -0300

    Redux implementation

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Card from "./Card";
 
 export default (props) => {
+  const { min, max } = props;
+  const aletoria = parseInt(Math.random() * (max - min)) + min;
+
   return (
     <Card title="Sorteio de um numero" purple>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>8</strong>
+          <strong>{aletoria}</strong>
         </span>
       </div>
     </Card>
