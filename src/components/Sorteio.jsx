44aa8e419cc124b1879c950571814a9ff48d3262
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-22 09:01:40 -0300

    Connect components with redux

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Card from "./Card";
+import { connect } from "react-redux";
 
-export default (props) => {
+const Sorteio = (props) => {
+  const { min, max } = props;
+
+  const random = parseInt(Math.random() * (max - min)) + min;
   return (
     <Card title="Sorteio de um numero" purple>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>{10}</strong>
+          <strong>{random}</strong>
         </span>
       </div>
     </Card>
   );
 };
+
+const mapStateToProps = (state) => {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+};
+
+export default connect(mapStateToProps)(Sorteio);
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,19 +1,13 @@
 // configurando todos redux e gerando store
-
 import { combineReducers, legacy_createStore as createStore } from "redux";
 
 const reducers = combineReducers({
   numeros: function (state, action) {
-    console.log(state, "", action);
+    //console.log(state, "", action);
     return {
-      min: 1,
-      max: 10,
+      min: 7,
+      max: 31,
     };
-  },
-  nomes: function (state, action) {
-    console.log(state, "", action);
-
-    return ["Ana", "Bia", "Carlos"];
   },
 });
 
