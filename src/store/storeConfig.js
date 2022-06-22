adf44ad1e73d869af2bc9fbaefca58ad5fa76e9a
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-22 08:24:04 -0300

    Redux settings

---
+++ src/store/storeConfig.js
@@ -0,0 +1,24 @@
+// configurando todos redux e gerando store
+
+import { combineReducers, legacy_createStore as createStore } from "redux";
+
+const reducers = combineReducers({
+  numeros: function (state, action) {
+    console.log(state, "", action);
+    return {
+      min: 1,
+      max: 10,
+    };
+  },
+  nomes: function (state, action) {
+    console.log(state, "", action);
+
+    return ["Ana", "Bia", "Carlos"];
+  },
+});
+
+function storeConfig() {
+  return createStore(reducers);
+}
+
+export default storeConfig;
